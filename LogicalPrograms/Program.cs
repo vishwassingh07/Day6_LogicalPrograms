585229b9e25310fa623ef7cf3bf5a2995a245e9a
Vishwas <baghel.vishwas0@gmail.com>
2022-06-08 00:51:58 +0530

    [Vishwas]add: Fibonacci Series Program Added

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -6,6 +6,26 @@ namespace LogicalPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Logical Programs......");
+            bool end = true;
+            Console.WriteLine("1. Fibonacci Series\n2. End the program");
+            while (end == true)
+            {
+                Console.WriteLine("Choose an option to execute.....");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FibonacciSeries series = new FibonacciSeries();
+                        series.Fibonacci();
+                        break;
+                    case 2:
+                        end = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please choose a valid option");
+                        break;
+                }
+            }
         }
     }
 }
